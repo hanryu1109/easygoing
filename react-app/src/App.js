18d00ad929f2f6eef1a98abd02ea22f58d3cb9b1
hanryu1109 <hanryu1109@gmail.com>
2021-07-23 10:51:53 +0900

    컴포넌트 만들기 첫번째

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+          <h1>Web</h1>
+          World Wide Web!
+      </header>   
+    );
+  }
+}
+
+
+// 7 line ~ 16 line이 컴포넌트를 만드는 곳
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React!!!
+        <Subject></Subject>
       </div>
       // 리턴된 값 중 가장 바깥태그는 하나여야만 한다! 
     )
