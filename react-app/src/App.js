81518f54e2773697f05e3b81f493433648fb41d8
Areum Han <hanryu1109@gmail.com>
2021-07-23 19:14:24 +0900

    state 공부 중...

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Contents';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-          <h1>Web</h1>
-          World Wide Web!
-      </header>   
-    );
-  }
-}
-
-
-// 7 line ~ 16 line이 컴포넌트를 만드는 곳
+// 8 line ~ 19 line이 컴포넌트를 만드는 곳
 class App extends Component {
+  //컴포넌트가 실행될 때 constructor() 함수가 있다면 걔가 젤 먼저 실행되고 초기화를 담당한다.
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'Web', sub: 'World Wide Web'},
+      contents: [
+        {id: 1, title:'HTML', desc:'HTML is Hyper Text...'},
+        {id: 2, title:'CSS', desc:'CSS is Cascading Style...'},
+        {id: 3, title:'JavaScript', desc:'JavaScript is for interactive..'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC
+          data={this.state.contents}
+        ></TOC>
+        <Content title="HTML" desc="!HTML is HyperText Markup Language"></Content>
       </div>
       // 리턴된 값 중 가장 바깥태그는 하나여야만 한다! 
     )
@@ -28,3 +37,4 @@ class App extends Component {
 }
 
 export default App;
+//react : 정리정돈 도구로 보자!
