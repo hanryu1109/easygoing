06b6e9ad00ac4f1e1d3e18b2ec0b4cab72aaf7ac
Areum Han <hanryu1109@gmail.com>
2021-07-26 19:02:40 +0900

    component event 생성 공부중

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,8 +9,11 @@ class App extends Component {
   //컴포넌트가 실행될 때 constructor() 함수가 있다면 걔가 젤 먼저 실행되고 초기화를 담당한다.
   constructor(props) {
     super(props);
+    //state값 초기화
     this.state = {
+      mode: 'read',
       subject: {title: 'Web', sub: 'World Wide Web'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!'},
       contents: [
         {id: 1, title:'HTML', desc:'HTML is Hyper Text...'},
         {id: 2, title:'CSS', desc:'CSS is Cascading Style...'},
@@ -20,16 +23,50 @@ class App extends Component {
   }
 
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    console.log('render', this);
     return (
       <div className="App">
         <Subject 
-          title={this.state.subject.title}
+          title={this.state.subject.title} //리액트에서는 props를 따옴표를 붙여주면 문자가 되고 자바스크립 코드로서 실행되게 하고 싶다면 중괄호를 쓰면 된다.
           sub={this.state.subject.sub}
+          onChangePage={function() {
+            alert('hihi!!');
+            this.setState({
+              mode : "welcome"
+            });
+          }.bind(this)}
         ></Subject>
+        {/* <header>
+          <h1><a href="/" onClick={function(e) {
+            console.log(e);
+            e.preventDefault(); //기본 동작을 지워버린다.
+            // debugger;
+            // this.state.mode = 'welcome'; // 이벤트가 실행되는 이 함수 안에서는 this가 이 컴포넌트를 가리키는 것이 아니라 아무것도 가리키지 않는다.
+            this.setState({
+              mode: 'welcome',
+            })
+            // state값이 바뀔 때는 setState 함수로!
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>    */}
         <TOC
+          onChangePage={
+            function() {
+              alert('hellowwwww😋');
+              this.setState({mode: 'read'});
+            }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title="HTML" desc="!HTML is HyperText Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
       // 리턴된 값 중 가장 바깥태그는 하나여야만 한다! 
     )
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -2,20 +2,29 @@ import React, { Component } from "react";
 
 class TOC extends Component {
     render() {
+        console.log('TOC render')
+
         //this.props.data 를 가지고 있다.
         var lists = [];
         var data = this.props.data;
         var i = 0;
         while (i < data.length) {
-            lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        onClick={function(e) {
+                            e.preventDefault();
+                            this.props.onChangePage();
+                        }.bind(this)}
+                    >{data[i].title}</a>
+                </li>);
             i += 1;
         }
         return (
             <nav>
             <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
+                {lists}
             </ul>
             </nav>  
         );
