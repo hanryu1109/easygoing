5f246afc13f37e17e43ab5e73c70ae747c07894c
Areum Han <hanryu1109@gmail.com>
2021-08-05 19:04:03 +0900

    Update index.js

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom"
 
 function Home() {
   return (
@@ -35,19 +38,19 @@ function App() {
   return (
     <div>
       <h1>React Router Dom Example</h1>
-      <Home></Home>
-      <Topics></Topics>
-      <Contact></Contact>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <Route exact path="/"><Home></Home></Route>
+        <Route path="/topics"><Topics></Topics></Route>
+        <Route path="/contact"><Contact></Contact></Route>
+      </ul>
     </div>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
